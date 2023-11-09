4730229313efc12634ddef9c287a93bf2081a560
JakovlevMA <lolipopp345@gmail.com>
2023-11-09 18:35:31 +0500

    initial commit

--- src/ResultTable.jsx
+++ src/ResultTable.jsx
@@ -1,21 +1,28 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+class ResultTable extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            tableData: []
+        };
+    }
 
-const ResultTable = ({ data }) => {
-    const [tableData, setTableData] = useState([]);
-    console.log(tableData);
-
-    useEffect(() => {
-        if (data) {
-            const newData = {
-                time: data.current.time.slice(11),
-                temperature: `${data.current.temperature_2m} ${data.current_units.temperature_2m}`,
-            };
-            setTableData((prevData) => [...prevData, newData]);
+    componentDidUpdate(prevProps) {
+        if (this.props.data !== prevProps.data) {
+            if (this.props.data) {
+                const newData = {
+                    time: this.props.data.current.time.slice(11),
+                    temperature: `${this.props.data.current.temperature_2m} ${this.props.data.current_units.temperature_2m}`
+                };
+                this.setState((prevState) => ({
+                    tableData: [...prevState.tableData, newData]
+                }));
+            }
         }
-    }, [data]);
+    }
 
-    function createResLocation() {
-        return tableData.map((item, index) => (
+    createResLocation() {
+        return this.state.tableData.map((item, index) => (
             <tr key={index}>
                 <td>{item.time}</td>
                 <td>{item.temperature}</td>
@@ -23,22 +30,23 @@ const ResultTable = ({ data }) => {
         ));
     }
 
-    return (
-        <div>
-            {tableData.length > 0 && (
-                <table>
-                    <tbody>
-                    <tr>
-                        <th>Time</th>
-                        <th>Value</th>
-                    </tr>
-                    {createResLocation()}
-                    </tbody>
-                </table>
-            )}
-        </div>
-    );
-};
-
+    render() {
+        return (
+            <div>
+                {this.state.tableData.length > 0 && (
+                    <table>
+                        <tbody>
+                        <tr>
+                            <th>Time</th>
+                            <th>Value</th>
+                        </tr>
+                        {this.createResLocation()}
+                        </tbody>
+                    </table>
+                )}
+            </div>
+        );
+    }
+}
 
 export default ResultTable;
--- src/TableData.jsx
+++ src/TableData.jsx
@@ -1,83 +1,93 @@
-import React, { useState } from "react";
+import React from "react";
 import ResultTable from "./ResultTable";
 
-const TableData = () => {
-    const [latitude, setLatitude] = useState("");
-    const [longitude, setLongitude] = useState("");
-    const [locations, setLocations] = useState([]);
-    const [data, setData] = useState(null);
+class TableData extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            latitude: "",
+            longitude: "",
+            locations: [],
+            data: null
+        };
+    }
 
-    async function fetchData() {
+    fetchData = async () => {
         let response = await fetch(
-            `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m`
+            `https://api.open-meteo.com/v1/forecast?latitude=${this.state.latitude}&longitude=${this.state.longitude}&current=temperature_2m`
     );
         let data = await response.json();
-        setData(data);
+        this.setState({ data: data });
     }
 
-    function addNewLocation() {
-        fetchData();
-        const newLocaton = {
-            latitude,
-            longitude,
+    addNewLocation = () => {
+        this.fetchData();
+        const newLocation = {
+            latitude: this.state.latitude,
+            longitude: this.state.longitude
         };
-        setLocations([...locations, newLocaton]);
-        setLatitude("");
-        setLongitude("");
+        this.setState({
+            locations: [...this.state.locations, newLocation],
+            latitude: "",
+            longitude: ""
+        });
     }
 
-    function removeLocation(index) {
-        const updatedLocations = [...locations];
+    removeLocation = (index) => {
+        const updatedLocations = [...this.state.locations];
         updatedLocations.splice(index, 1);
-        setLocations(updatedLocations);
+        this.setState({ locations: updatedLocations });
     }
-    return (
-        <div className="container">
-            <table>
-                <tbody>
-                <tr>
-                    <th>Location</th>
-                    <th>Control</th>
-                </tr>
-                {locations.map((location, index) => (
-                    <tr key={index}>
+
+    render() {
+        return (
+            <div className="container">
+                <table>
+                    <tbody>
+                    <tr>
+                        <th>Location</th>
+                        <th>Control</th>
+                    </tr>
+                    {this.state.locations.map((location, index) => (
+                        <tr key={index}>
+                            <td>
+                                <span>{location.latitude}</span>
+                                <br />
+                                <span>{location.longitude}</span>
+                            </td>
+                            <td>
+                                <button onClick={() => this.removeLocation(index)}>
+                                    Remove Location
+                                </button>
+                            </td>
+                        </tr>
+                    ))}
+                    <tr>
                         <td>
-                            <span>{location.latitude}</span>
+                            <input
+                                type="text"
+                                placeholder="latitude"
+                                value={this.state.latitude}
+                                onChange={(e) => this.setState({ latitude: e.target.value })}
+                            />
                             <br />
-                            <span>{location.longitude}</span>
+                            <input
+                                type="text"
+                                placeholder="longitude"
+                                value={this.state.longitude}
+                                onChange={(e) => this.setState({ longitude: e.target.value })}
+                            />
                         </td>
                         <td>
-                            <button onClick={() => removeLocation(index)}>
-                                Remove Location
-                            </button>
+                            <button onClick={() => this.addNewLocation()}>Add Location</button>
                         </td>
                     </tr>
-                ))}
-                <tr>
-                    <td>
-                        <input
-                            type="text"
-                            placeholder="latitude"
-                            value={latitude}
-                            onChange={(e) => setLatitude(e.target.value)}
-                        />
-                        <br />
-                        <input
-                            type="text"
-                            placeholder="longitude"
-                            value={longitude}
-                            onChange={(e) => setLongitude(e.target.value)}
-                        />
-                    </td>
-                    <td>
-                        <button onClick={() => addNewLocation()}>Add Location</button>
-                    </td>
-                </tr>
-                </tbody>
-            </table>
-            <ResultTable data={data} />
-        </div>
-    );
-};
+                    </tbody>
+                </table>
+                <ResultTable data={this.state.data} />
+            </div>
+        );
+    }
+}
 
 export default TableData;
